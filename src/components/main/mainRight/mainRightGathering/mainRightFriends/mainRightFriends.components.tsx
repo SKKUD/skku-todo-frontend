a2fcd3998c16cb036a954ed4e919433148808120
ajs3801 <tt14an@gmail.com>
2023-10-14 22:32:06 +0900

    add gathering icon and title

--- src/components/main/mainRight/mainRightGathering/mainRightFriends/mainRightFriends.components.tsx
+++ src/components/main/mainRight/mainRightGathering/mainRightFriends/mainRightFriends.components.tsx
@@ -4,31 +4,19 @@ import {
   MainRightFriendsWrapper,
   MainRightFriendsIcon,
   MainRightAddFriendsBtn,
+  MainRightAddFriendsBtnContainer,
 } from "./mainRightFriends.styles";
 
 import Img from "../../../../../assets/images/profile.jpeg";
+import GatheringPlus from "../../../../../assets/images/GatheringPlus.svg";
 
 const MainRightFriends = () => {
   return (
     <MainRightFriendsWrapper>
       <MainRightFriendsIcon src={Img} />
-      <MainRightAddFriendsBtn>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 30 30"
-          fill="none"
-        >
-          <path
-            d="M27 15H3M15 3V27"
-            stroke="#5F5F5F"
-            stroke-width="5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </MainRightAddFriendsBtn>
+      <MainRightAddFriendsBtnContainer>
+        <MainRightAddFriendsBtn className="add_friends_btn" src={GatheringPlus} />
+      </MainRightAddFriendsBtnContainer>
     </MainRightFriendsWrapper>
   );
 };
