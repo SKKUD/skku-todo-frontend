6e9f1bcbdc80fde7cce14cc8068c1185d4194bcc
ajs3801 <tt14an@gmail.com>
2023-10-19 09:51:38 +0900

    raw calendar

--- src/components/main/mainLeft/mainLeftCalendar/mainLeftCalendar.components.tsx
+++ src/components/main/mainLeft/mainLeftCalendar/mainLeftCalendar.components.tsx
@@ -1,33 +1,144 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState } from 'react';
 
 // import styled components
 import { 
   MainLeftCalendarContainer,
-  MainLeftCalendarBox,
-  MainLeftCalendarContent,
+  CalendarDaysContainer,
+  CalendarCellsContainer,
+  CalendarHeaderContainer,
+  CalendarCellRows
 } from "./mainLeftCalendar.styles";
 
-// type for Calendar
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+import { format, addMonths, subMonths } from 'date-fns';
+import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from 'date-fns';
+import { isSameMonth, isSameDay, addDays, parse } from 'date-fns';
+
+interface IRenderHeader {
+  currentMonth: any;
+  prevMonth: any;
+  nextMonth: any;
+}
+
+const RenderHeader = ({ currentMonth, prevMonth, nextMonth }: IRenderHeader) => {
+  return (
+      <CalendarHeaderContainer>
+          <div className="col col-start">
+              <span className="text">
+                  <span className="text month">
+                      {format(currentMonth, 'M')}월
+                  </span>
+                  {format(currentMonth, 'yyyy')}
+              </span>
+          </div>
+          <div className="col col-end">
+              <div onClick={prevMonth}>이전</div>
+              <div onClick={nextMonth}>이후</div>
+          </div>
+      </CalendarHeaderContainer>
+  );
+};
+
+const RenderDays = () => {
+  const days = [];
+  const date = ['Sun', 'Mon', 'Thu', 'Wed', 'Thrs', 'Fri', 'Sat'];
+
+  for (let i = 0; i < 7; i++) {
+      days.push(
+          <div className="col" key={i}>
+              {date[i]}
+          </div>,
+      );
+  }
+
+  return <CalendarDaysContainer>{days}</CalendarDaysContainer>;
+};
+
+interface IRenderCells {
+  currentMonth: any;
+  selectedDate: any;
+  onDateClick: any;
+}
+
+const RenderCells = ({ currentMonth, selectedDate, onDateClick }: IRenderCells) => {
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = '';
+
+  while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+          formattedDate = format(day, 'd');
+          const cloneDay = day;
+          days.push(
+              <div
+                  className={`col cell ${
+                      !isSameMonth(day, monthStart)
+                          ? 'disabled'
+                          : isSameDay(day, selectedDate)
+                          ? 'selected'
+                          : format(currentMonth, 'M') !== format(day, 'M')
+                          ? 'not-valid'
+                          : 'valid'
+                  }`}
+                  // key={day}
+                  // onClick={() => onDateClick(parse(cloneDay))}
+              >
+                  <span
+                      className={
+                          format(currentMonth, 'M') !== format(day, 'M')
+                              ? 'text not-valid'
+                              : ''
+                      }
+                  >
+                      {formattedDate}
+                  </span>
+              </div>,
+          );
+          day = addDays(day, 1);
+      }
+      rows.push(
+          <CalendarCellRows>
+              {days}
+          </CalendarCellRows>,
+      );
+      days = [];
+  }
+  return <CalendarCellsContainer>{rows}</CalendarCellsContainer>;
+};
 
 const MainLeftCalendar = () => {
-  // useState 훅의 초기값으로 현재 날짜를 넣어줌
-  const [today, setToday] = useState(new Date()); 
+    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
-  // onChange 이벤트에 넣어줘서 날짜가 지날 때마다 today값이 업데이트 되도록 구현
-  const onChangeToday = () => {
-    setToday(today);
-  };
-
-  return (
-    <MainLeftCalendarContainer>
-      <MainLeftCalendarBox>
-        <MainLeftCalendarContent locale="en" onChange={onChangeToday} value={today} />
-      </MainLeftCalendarBox>
-    </MainLeftCalendarContainer>
-  )
+    const prevMonth = () => {
+        setCurrentMonth(subMonths(currentMonth, 1));
+    };
+    const nextMonth = () => {
+        setCurrentMonth(addMonths(currentMonth, 1));
+    };
+    const onDateClick = (day: any) => {
+        setSelectedDate(day);
+    };
+    return (
+      <MainLeftCalendarContainer className="calendar">
+          <RenderHeader
+              currentMonth={currentMonth}
+              prevMonth={prevMonth}
+              nextMonth={nextMonth}
+          />
+          <RenderDays />
+          <RenderCells
+              currentMonth={currentMonth}
+              selectedDate={selectedDate}
+              onDateClick={onDateClick}
+          />
+      </ MainLeftCalendarContainer>
+  );
 }
 
 export default MainLeftCalendar;
